ed8fda5616d98fbb7b0fca67281a5897adb1d64c
pragyat29 <itspragyatiwari.2913@gmail.com>
2026-06-22 00:45:20 +0530

    Time: 2 ms (91.91%) | Memory: 48 MB (19.64%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[left] = nums[i];
+                left++;
+            }
+        }
+        while(left < nums.length){
+            nums[left] = 0;
+            left++;
+        }
+    }
+}
